4a95d5b2937ddff950f9caca05c2179fabdee2a8
secondlifetechai <secondlifetechai@gmail.com>
2025-02-22 04:23:44 +0400

    changed default theme to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           enableSystem={true}
-          defaultTheme="system"
+          defaultTheme="dark"
         >
           <Aoscompo>
             <Providers>{children}</Providers>
